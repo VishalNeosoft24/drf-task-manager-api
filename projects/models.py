58336045176358b146d41e847ecfaf3613a2cc71
vishal <vneosoft@gmail.com>
2025-11-25 14:56:01 +0530

    create models for task

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectMember(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_memberships')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='members')
+
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.project.name}"
