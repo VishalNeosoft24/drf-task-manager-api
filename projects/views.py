ee24c57b02c8f9a7a57165f8005b111d065c0d28
vishal <vneosoft@gmail.com>
2025-12-10 18:52:53 +0530

    update ListProjectMembersView

--- projects/views.py
+++ projects/views.py
@@ -7,9 +7,10 @@ from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions, 
 
 from projects.permissions import IsProjectOwner, CanCreateProject, CanUpdateDeleteProject
 from projects.serializers import ProjectMemberAddSerializer, ProjectSerializer
+from tasks.models import Task
 from tasks.utils.pagination import TaskPagination
 from .models import Project, ProjectMember
-from django.db.models import Q
+from django.db.models import Q, Prefetch
 
 class ProjectListCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated, CanCreateProject]
@@ -19,7 +20,7 @@ class ProjectListCreateView(ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_staff or user.is_superuser:
+        if user.is_superuser:
             return Project.objects.all()
         # return Project.objects.filter(members__user=user).exclude(members__role ="viewer")
         return Project.objects.filter(members__user=user)
@@ -40,7 +41,7 @@ class ProjectListCreateView(ListCreateAPIView):
 
     def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        
+        print('queryset: ', queryset)
         role = request.query_params.get("role")
         if role:
             queryset = queryset.filter(project__id=role)
@@ -52,10 +53,18 @@ class ProjectListCreateView(ListCreateAPIView):
                 Q(description__icontains=search_filter) 
                 # Q(comments__icontains=search_filter)
             )
+        
+        project_search = request.query_params.get("projectsearch")
+        if project_search:
+            queryset = queryset.filter(
+                Q(name__icontains=project_search) |
+                Q(description__icontains=project_search)
+            )
+            print("Filtered Projects:", queryset)
         paginator = self.pagination_class()
         paginated_projects = paginator.paginate_queryset(queryset, request)
 
-        serializer = ProjectSerializer(paginated_projects, many=True)
+        serializer = ProjectSerializer(paginated_projects, many=True, context={'request': request})
 
         return paginator.get_paginated_response({
             "message": "Projects fetched successfully",
@@ -73,7 +82,7 @@ class ProjectRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_staff or user.is_superuser:
+        if user.is_superuser:
             return Project.objects.all()
         return Project.objects.filter(members__user=user)
 
@@ -109,21 +118,54 @@ class ListProjectMembersView(APIView):
 
     def get(self, request, pk):
         project = get_object_or_404(Project, id=pk)
-        members = ProjectMember.objects.filter(project=project).select_related("user")
 
-        project_members = [
-            {
-                "id": m.user.id,
-                "username": m.user.username,
-                "email": m.user.email,
-                "job_role": (m.user.job_role.replace("_", " ").title() if m.user.job_role else None),
-                "department": m.user.department,
-                "designation": m.user.designation
-            } for m in members
-        ]
+        # Prefetch tasks assigned to each user BUT only for this project.
+        # NOTE: Task.user.related_name == 'assigned_tasks' in your Task model.
+        members_qs = (
+            ProjectMember.objects.filter(project=project)
+            .select_related("user")
+            .prefetch_related(
+                Prefetch(
+                    "user__assigned_tasks",                       # related_name on Task.user
+                    queryset=Task.objects.filter(project=project).order_by('-created_at')[:10],# only tasks of this project
+                    to_attr="project_tasks"                       # will be available as user.project_tasks
+                )
+            )
+        )
+
+        project_members = []
+        for m in members_qs:
+            user = m.user
+            # get tasks prefetched for this user for the current project
+            tasks_for_user = getattr(user, "project_tasks", [])
+
+            project_members.append({
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "job_role": (user.job_role.replace("_", " ").title() if user.job_role else None),
+                "department": user.department,
+                "designation": user.designation,
+                "project_role": m.role,
+                "joined_at": m.joined_at,
+                "tasks": [
+                    {
+                        "id": t.id,
+                        "name": t.name,
+                        "status": t.status,
+                        "priority": t.priority,
+                        "due_date": t.due_date,
+                    } for t in tasks_for_user
+                ],
+                "tasks_count": len(tasks_for_user),
+            })
 
         return Response(
-            {"project_id": project.id, "members": project_members},
+            {
+                "project_id": project.id,
+                "members_count": len(project_members),
+                "members": project_members
+            },
             status=status.HTTP_200_OK
         )
 
@@ -138,7 +180,7 @@ class RemoveProjectMemberView(APIView):
             project_id=project_id
         ).first()        
         if member.user == request.user:
-            return Response({"errpr":"You can not delete your self."}, status=status.HTTP_403_FORBIDDEN)
+            return Response({"error":"You can not delete your self."}, status=status.HTTP_403_FORBIDDEN)
         if not member:
             return Response({"error": "Member not found"}, status=status.HTTP_404_NOT_FOUND)
         
